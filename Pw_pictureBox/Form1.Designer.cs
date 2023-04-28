aa9a8676e4c84ea6b98b735717c283bbbb5469b2
GiovaniXX <giovani_chaves@hotmail.com>
2023-04-28 18:52:25 -0300

    Adicionar arquivos de projeto.

---
+++ Pw_pictureBox/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Pw_pictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_surface = new System.Windows.Forms.ComboBox();
+            this.pictureBox_surface = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_surface)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Surface:";
+            // 
+            // comboBox_surface
+            // 
+            this.comboBox_surface.FormattingEnabled = true;
+            this.comboBox_surface.Location = new System.Drawing.Point(104, 154);
+            this.comboBox_surface.Name = "comboBox_surface";
+            this.comboBox_surface.Size = new System.Drawing.Size(317, 21);
+            this.comboBox_surface.TabIndex = 1;
+            this.comboBox_surface.TextChanged += new System.EventHandler(this.Load_Surface);
+            // 
+            // pictureBox_surface
+            // 
+            this.pictureBox_surface.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox_surface.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_surface.Location = new System.Drawing.Point(291, 18);
+            this.pictureBox_surface.Name = "pictureBox_surface";
+            this.pictureBox_surface.Size = new System.Drawing.Size(130, 130);
+            this.pictureBox_surface.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_surface.TabIndex = 2;
+            this.pictureBox_surface.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.pictureBox_surface);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox_surface);
+            this.groupBox1.Location = new System.Drawing.Point(451, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(427, 523);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ITEM";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.SkyBlue;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(433, 523);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(890, 562);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Perfect World sGSHOP 2023 Teste";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_surface)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox_surface;
+        private System.Windows.Forms.PictureBox pictureBox_surface;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Pw_pictureBox/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Pw_pictureBox
+{
+    public partial class Form1 : Form
+    {
+        private string directoryPath = @"E:\PROJETOS DEV-GIOVANI\PROJETOS VISUAL STUDIO 2022\C#\Pw_pictureBox\Pw_pictureBox\bin\Debug\surfaces\百宝阁\";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadComboBoxItems();
+        }
+
+        private void LoadComboBoxItems()
+        {
+            // Adiciona os arquivos do diretório no ComboBox
+            string[] files = Directory.GetFiles(directoryPath);
+            foreach (string file in files)
+            {
+                comboBox_surface.Items.Add(Path.GetFileNameWithoutExtension(file));
+            }
+        }
+
+        private void Load_Surface(object sender, EventArgs e)
+        {
+            // Verifica se foi selecionado algum item no ComboBox
+            if (comboBox_surface.SelectedIndex != -1)
+            {
+                // Obtém o caminho completo do arquivo de imagem
+                string imagePath = Path.Combine(directoryPath, comboBox_surface.SelectedItem.ToString() + ".jpg");
+
+                // Verifica se o arquivo de imagem existe
+                if (File.Exists(imagePath))
+                {
+                    // Carrega a imagem no PictureBox
+                    Image image = Image.FromFile(imagePath);
+                    pictureBox_surface.Image = image;
+                }
+                else
+                {
+                    MessageBox.Show("O arquivo de imagem selecionado não existe.");
+                }
+            }
+        }
+    }
+}
