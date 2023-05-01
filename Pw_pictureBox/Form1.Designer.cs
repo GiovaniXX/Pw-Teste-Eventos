9e2ef0c4cae9e6f3da6078d427fae567a52c713a
GiovaniXX <giovani_chaves@hotmail.com>
2023-05-01 18:34:19 -0300

    ...

--- Pw_pictureBox/Form1.Designer.cs
+++ Pw_pictureBox/Form1.Designer.cs
@@ -33,13 +33,18 @@
             this.comboBox_surface = new System.Windows.Forms.ComboBox();
             this.pictureBox_surface = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_surface)).BeginInit();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -75,6 +80,10 @@
             // 
             this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
@@ -88,6 +97,37 @@
             this.groupBox1.TabIndex = 3;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "ITEM";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Navy;
+            this.label4.Location = new System.Drawing.Point(6, 477);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(378, 26);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Системный разработчик и веб-дизайнер...\r\nИзучение программной инженерии с ожидаем" +
+    "ым выпуском в 2027 году.\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Navy;
+            this.label3.Location = new System.Drawing.Point(6, 518);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "系統開發人員和網頁設計師";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(6, 545);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(397, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "2023™ 01000111 01101001 01101111 01110110 01100001 01101110 01101001";
             // 
             // linkLabel1
             // 
@@ -111,36 +151,45 @@
             this.textBox1.TabIndex = 4;
             this.textBox1.Text = resources.GetString("textBox1.Text");
             // 
-            // label2
+            // label5
             // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.Navy;
-            this.label2.Location = new System.Drawing.Point(6, 545);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(397, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "2023™ 01000111 01101001 01101111 01110110 01100001 01101110 01101001";
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.ForeColor = System.Drawing.Color.Navy;
+            this.label5.Location = new System.Drawing.Point(6, 450);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(232, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Код полностью реализован на языке csharp";
             // 
-            // label3
+            // label6
             // 
-            this.label3.AutoSize = true;
-            this.label3.ForeColor = System.Drawing.Color.Navy;
-            this.label3.Location = new System.Drawing.Point(6, 518);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(151, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "系統開發人員和網頁設計師";
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.ForeColor = System.Drawing.Color.Navy;
+            this.label6.Location = new System.Drawing.Point(6, 426);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(165, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "完全用 csharp 語言實現的代碼";
             // 
-            // label4
+            // pictureBox1
             // 
-            this.label4.AutoSize = true;
-            this.label4.ForeColor = System.Drawing.Color.Navy;
-            this.label4.Location = new System.Drawing.Point(6, 477);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(378, 26);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Системный разработчик и веб-дизайнер...\r\nИзучение программной инженерии с ожидаем" +
-    "ым выпуском в 2027 году.\r\n";
+            this.pictureBox1.Image = global::Pw_pictureBox.Properties.Resources.Visual_Studio;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 353);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 388);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Visual Studio 2022";
             // 
             // Form1
             // 
@@ -158,6 +207,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox_surface)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -174,6 +224,10 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
     }
 }
 
